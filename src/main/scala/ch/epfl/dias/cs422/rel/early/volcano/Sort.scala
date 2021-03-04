c322d247a8ab7122d75694cb915f189e31f9f6c0
saradj <sara.djambazovska@epfl.ch>
2021-03-04 09:59:09 +0100

    agg performance

--- src/main/scala/ch/epfl/dias/cs422/rel/early/volcano/Sort.scala
+++ src/main/scala/ch/epfl/dias/cs422/rel/early/volcano/Sort.scala
@@ -31,12 +31,14 @@ class Sort protected (
     */
   override def open(): Unit = {
     sorted_input = SortedSet()
-    for (tuple <- input.iterator) { // .toList.slice(offset.getOrElse(0), offset.getOrElse(0) + fetch.getOrElse(0)) ON THE ITTER to optimize TODO
+    val full = input.iterator.toList.slice(offset.getOrElse(0), offset.getOrElse(0) + fetch.getOrElse(Integer.MAX_VALUE))
+    for (tuple <- full) { // .toList.slice(offset.getOrElse(0), offset.getOrElse(0) + fetch.getOrElse(0)) ON THE ITTER to optimize TODO
       sorted_input += Sorted(tuple, collation)
     }
-    sorted_input = sorted_input.slice(
-      offset.getOrElse(0),
-      offset.getOrElse(0) + fetch.getOrElse(Integer.MAX_VALUE))
+    sorted_input = sorted_input
+//      .slice(
+//      offset.getOrElse(0),
+//      offset.getOrElse(0) + fetch.getOrElse(Integer.MAX_VALUE))
     iter = sorted_input.iterator
 
   }
